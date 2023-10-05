76d14ded102a0e2ca2cfe0de35a80d82b99d5309
mariacarai <147046035+mariacarai@users.noreply.github.com>
2023-10-05 12:42:31 +0300

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,14 @@
+var video = document.getElemenetById("video");
+//--------------
+function on_cam_succes(stream){video.srcObject=stream;
+}
+//--------------
+function on_cam_error(err){
+alert("error."+err.message);}
+//--------------var constraints = {audio: false, video:true};
+navigator.mediaDevices.getUserMedia(constraints).then(on_cam_success)
+.catch(on_cam_error);//--------------
+function capteaza(){
+var c = document.getElementById("canvas");c.width = video.width;
+c.height = video.height;var ctx = c.getContext("2d");
+ctx.drawImage(video, 0, 0, 640, 480);}
